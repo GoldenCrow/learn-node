6414898274c4f4f30f4223ee6cb0ab91586de9ec
GoldenCrow <peter.stevenson1814@gmail.com>
2017-05-14 14:14:55 +0100

    Middleware

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,12 @@
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Wes';
+  // res.cookie('name', 'Wes is cool', { maxAge: 90000000 });
+  if (req.name === 'Wes') {
+    throw Error('That is a stupid name');
+  }
+  next();
+}
 exports.homePage = (req, res) => {
+  console.log(req.name)
   res.render('index');
 }
