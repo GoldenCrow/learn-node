a5deb61c4556d388d82f4d08a94306b338ccb67a
GoldenCrow <peter.stevenson1814@gmail.com>
2017-05-14 12:43:00 +0100

    MVC

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,23 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController')
 // Do work here
-router.get('/', (req, res) => {
-  // const wes = { name: 'Wes', age: 100, cool: true };
-  // res.send('Hey! It works!');
-  // res.json(wes);
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-    name: 'Wes',
-    dog: req.query.dog,
-    title: 'I love food'
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-})
+router.get('/', storeController.homePage);
 
 module.exports = router;
