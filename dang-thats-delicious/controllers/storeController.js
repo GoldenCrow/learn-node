4dbc8109a621ed2fd7db8ef151def37e1da5fb8f
GoldenCrow <peter.stevenson1814@gmail.com>
2017-05-16 22:25:46 +0100

    Uploading and Resizing with Middleware

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,8 +1,22 @@
 const moongoose = require('mongoose');
 const Store = moongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if(isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: `That filetype isn't allowed!`}, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index');
 }
 
@@ -10,6 +24,24 @@ exports.addStore = (req, res) => {
   res.render('editStore', {
     title: '💩 Add Store'
   });
+}
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to our filesystem, keep going
+  next();
 }
 
 exports.createStore = async (req, res) => {
@@ -34,15 +66,12 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
-  // set the location data to be a point
-  rq.body.location.type = 'Point';
   // find and update the store
-  const store = await Store.findOneAndUpdate({ _id: req.params.id },
-  req.body, {
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // return the new store instead of the old one
     runValidators: true
   }).exec();
-  // Redirect them to the store and tell them it worked
-  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
   res.redirect(`/stores/${store._id}/edit`);
+  // Redriect them the store and tell them it worked
 }
