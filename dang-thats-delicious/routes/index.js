49c07d32daa52e2edd539f7302bd4b34239524ec
GoldenCrow <peter.stevenson1814@gmail.com>
2017-05-18 23:42:12 +0100

    Virtual Fields, Login:Logout middleware and Protecting Routes

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -8,7 +8,9 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add',
+  authController.isLoggedIn,
+  storeController.addStore);
 
 router.post('/add',
   storeController.upload,
@@ -29,6 +31,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
@@ -38,6 +41,8 @@ router.post('/register',
   userController.validateRegister,
   userController.register,
   authController.login
-)
+);
+
+router.get('/logout', authController.logout);
 
 module.exports = router;
