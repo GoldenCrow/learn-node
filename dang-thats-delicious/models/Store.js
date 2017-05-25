87eb02a32eecdc2a2000756b6545588601eabed1
GoldenCrow <peter.stevenson1814@gmail.com>
2017-05-25 13:30:13 +0100

    Advanced Aggregation

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -39,9 +39,9 @@ const storeSchema = new mongoose.Schema({
     required: 'You must supply an author'
   }
 }, {
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true }
-});
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  });
 
 // Define our indexes
 storeSchema.index({
@@ -51,7 +51,7 @@ storeSchema.index({
 
 storeSchema.index({ location: '2dsphere' });
 
-storeSchema.pre('save', async function(next) {
+storeSchema.pre('save', async function (next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop this function from running
@@ -68,11 +68,32 @@ storeSchema.pre('save', async function(next) {
   // TODO make more resiliant so slugs are unique
 });
 
-storeSchema.statics.getTagsList = function() {
+storeSchema.statics.getTagsList = function () {
   return this.aggregate([
     { $unwind: '$tags' },
     { $group: { _id: '$tags', count: { $sum: 1 } } },
     { $sort: { count: -1 } }
+  ]);
+}
+
+storeSchema.statics.getTopStores = function () {
+  return this.aggregate([
+    // Lookup Stores and populate their reviews
+    { $lookup: { from: 'reviews', localField: '_id', foreignField: 'store', as: 'reviews' }},
+    // filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true } }},
+    // Add the average reviews field
+    { $project: {
+      photo: '$$ROOT.photo',
+      name: '$$ROOT.name',
+      reviews: '$$ROOT.reviews',
+      slug: '$$ROOT.slug',
+      averageRating: { $avg: '$reviews.rating' }
+    }},
+    // sort it by our new field, highest reviews first
+    { $sort: { averageRating: -1 }},
+    // limit to at most 10
+    { $limit: 10 }
   ]);
 }
 
@@ -82,5 +103,13 @@ storeSchema.virtual('reviews', {
   localField: '_id', // which field on the store?
   foreignField: 'store' // which field on the review?
 });
+
+function autopopulate(next) {
+  this.populate('reviews');
+  next();
+}
+
+storeSchema.pre('find', autopopulate);
+storeSchema.pre('findOne', autopopulate);
 
 module.exports = mongoose.model('Store', storeSchema);
